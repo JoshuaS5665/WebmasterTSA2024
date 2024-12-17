623a34aa4ae5d08fefda40a49fb8de13eac061ad
jshah266507 <30695273-jshah266507@users.noreply.replit.com>
2024-12-17 13:02:53 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
+  console.log("Home get req is working");
+});
+
+app.get("/winterMenu", (req, res) => {
+  res.status(200).sendFile(__dirname + "/menu/seasonalMenus/winterMenu.html");
+  console.log("Menus is running");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
+});
---
+++ script.js
@@ -0,0 +1,78 @@
+function loadHead(url) {
+    const header = document.getElementById("header");
+    fetch(url)
+      .then((response) => response.text())
+      .then((data) => {
+        header.innerHTML = data;
+        console.log(url + "Function is Running");
+      });
+}
+
+function changeImages(beginning) {
+  const homeImage = document.getElementById("homeImage");
+  console.log(homeImage.src);
+
+  let x = 0;
+
+  setInterval(() => {
+    homeImage.classList.toggle("fade-out");
+    setTimeout(() => {
+      homeImage.classList.add("fade-out");
+      if (x == 0) {
+        homeImage.src = `${beginning}public/images/homeImage1.jpg`;
+        x++;
+      } else if (x == 1) {
+        homeImage.src = `${beginning}public/images/homeImage2.jpg`;
+        x++;
+      } else {
+        homeImage.src = `${beginning}public/images/homeImage3.jpg`;
+        x = 0;
+      }
+      homeImage.classList.remove("fade-out");
+    }, 1000);
+  }, 4000);
+}
+
+var acc = document.getElementsByClassName("faqButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display + "block";
+    }
+  });
+}
+
+/*function createFAQResponses(response, responseID, containerID) {
+  let FAQResponse;
+  const container = document.getElementById(containerID);
+  FAQResponse = document.createElement("h3");
+  FAQResponse.innerHTML = response;
+  FAQResponse.className = "faqresponse";
+  FAQResponse.id = responseID;
+  if (container) {
+    container.appendChild(FAQResponse);
+  } else {
+    console.error("Cannot find it");
+  }
+}
+
+function deleteFAQResponses(container, child) {
+  container.removeChild(child);
+}
+
+function toggleFAQResponses(response, responseID, containerID) {
+  let child = document.getElementById(responseID);
+  let container = document.getElementById(containerID);
+  if (child) {
+    deleteFAQResponses(container, child);
+  } else {
+    createFAQResponses(response, responseID, containerID);
+  }
+  
+}*/
