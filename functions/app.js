2c7091985526574d149bbf0614ca38279b2ced34
Akshay Lath <104273819+akshay23232@users.noreply.github.com>
2025-01-10 17:58:08 +0000

    Restored to '067bba896648b44c8dfa8ebf229487cf28d188c7'
    Replit-Restored-To:067bba896648b44c8dfa8ebf229487cf28d188c7

--- functions/app.js
+++ functions/app.js
@@ -4,7 +4,6 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 const PORT = 80;
-const path = require('path');
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -14,93 +13,121 @@ app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Main routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  console.log(__dirname + "/../../public/faq/faqs.html");
+  res.status(200).sendFile(__dirname + "/../public/index.html");
+  console.log("Home get req is working");
 });
 
 app.get("/index.html", (req, res) => {
+  console.log("Home page redirect is working");
   res.redirect(301, "/");
 });
 
-// FAQ routes
 app.get("/faqs", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/faq/faqs.html"));
+  res.sendFile(__dirname + "/../public/faq/faqs.html");
 });
 
 app.get("/faqs.html", (req, res) => {
   res.redirect(301, "/faqs");
 });
 
-// About routes
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/about/about.html"));
+  res.sendFile(__dirname + "../public/about/about.html");
 });
 
 app.get("/about.html", (req, res) => {
   res.redirect(301, "/about");
 });
 
-// Contact routes
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/contact/contact.html"));
+  res.sendFile(__dirname + "../public/contact/contact.html");
 });
 
 app.get("/contact/contactResponse", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/contact/contactResponse.html"));
+  res.sendFile(__dirname + "/public/contact/contactResponse.html");
+  console.log("Contact response route hit");
 });
 
-// Menu routes
+app.get("/contact.html", (req, res) => {
+  res.redirect(301, "/contact");
+});
+
 app.get("/menus", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/menu/menuHome.html"));
+  res.sendFile(__dirname + "/public/menu/menuHome.html");
+});
+
+app.get("/menuHome.html", (req, res) => {
+  res.redirect(301, "/menus");
 });
 
 app.get("/menus/winter", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/menu/seasonalMenus/winterMenu.html"));
+  res.sendFile(__dirname + "/public/menu/seasonalMenus/winterMenu.html");
 });
 
 app.get("/menus/spring", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/menu/seasonalMenus/springMenu.html"));
+  res.sendFile(__dirname + "/public/menu/seasonalMenus/springMenu.html");
 });
 
 app.get("/menus/summer", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/menu/seasonalMenus/summerMenu.html"));
+  res.sendFile(__dirname + "/public/menu/seasonalMenus/summerMenu.html");
 });
 
 app.get("/menus/fall", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/menu/seasonalMenus/fallMenu.html"));
+  res.sendFile(__dirname + "/public/menu/seasonalMenus/fallMenu.html");
 });
 
-// Mission routes
 app.get("/mission", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/mission/mission.html"));
+  res.sendFile(__dirname + "/public/mission/mission.html");
 });
 
-// Reservation routes
+app.get("/mission.html", (req, res) => {
+  res.redirect(301, "/mission");
+});
+
 app.get("/reservation", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/reservation/reservation.html"));
+  res.sendFile(__dirname + "/public/reservation/reservation.html");
+});
+
+app.get("/reservation.html", (req, res) => {
+  res.redirect(301, "/reservation");
 });
 
 app.get("/reservationsecond", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/reservation/reservation2.html"));
+  res.sendFile(__dirname + "/public/reservation/reservation2.html");
+});
+
+app.get("/public/reservation/reservation2.html", (req, res) => {
+  res.redirect(301, "/reservationsecond");
 });
 
 app.get("/reservation/final", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/reservation/bookTable.html"));
+  res.sendFile(__dirname + "/public/reservation/bookTable.html");
+});
+
+app.get("/public/reservation/bookTable.html", (req, res) => {
+  res.redirect(301, "/reservation/final");
 });
 
 app.get("/reservation/confirmation", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/reservation/reservationConfirmation.html"));
+  res.sendFile(__dirname + "/public/reservation/reservationConfirmation.html");
+});
+app.get("/public/reservation/reservationConfirmation.html", (req, res) => {
+  res.redirect(301, "/reservation/confirmation");
 });
 
-// Sources route
 app.get("/sources", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/requirements/sources.html"));
+  res.sendFile(__dirname + "/public/requirements/sources.html");
+});
+app.post("/", (req, res) => {
+  //const data = req.body;
+  //console.log("This is my data: " + data);
+  //res.json({message: "Your data has been received", data});
+  console.log("HELLO WORLD");
 });
 
-// Handle 404
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "../public/404/404.html"));
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/404/404.html");
 });
 
 app.use("/.netlify/functions/app", router);
